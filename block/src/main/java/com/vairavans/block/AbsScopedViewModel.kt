8b293dc97cc96774c368103f82864b8870334b35
Vairavan Srinivasan <vairav@codeaurora.org>
2019-04-11 23:34:56 -0400

    Fix AbsScopedViewModel to use SupervisorJob

--- block/src/main/java/com/vairavans/block/AbsScopedViewModel.kt
+++ block/src/main/java/com/vairavans/block/AbsScopedViewModel.kt
@@ -4,18 +4,18 @@ import androidx.annotation.CallSuper
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 
 /**
  * A [ViewModel] for Structured concurrency.
  */
 abstract class AbsScopedViewModel constructor(dispatcher : CoroutineDispatcher) : ViewModel() {
 
-    private val viewModelJob = Job()
-    protected val viewModelScope = CoroutineScope(dispatcher + viewModelJob)
+    protected val viewModelScope = CoroutineScope(dispatcher + SupervisorJob() )
 
     @CallSuper
     override fun onCleared() {
-        viewModelJob.cancel()
+        viewModelScope.cancel()
     }
 }
--- block/src/test/java/com/vairavans/block/AbsScopedViewModelTest.kt
+++ block/src/test/java/com/vairavans/block/AbsScopedViewModelTest.kt
@@ -2,8 +2,12 @@ package com.vairavans.block
 
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,6 +25,35 @@ class AbsScopedViewModelTest {
 
         val isScopeActive : Boolean
             get() = viewModelScope.isActive
+
+        var backgroundWorkCanceled : Boolean = false
+
+        var backgroundWorkCompleted : Boolean = false
+
+        fun doSomeCancelableBackgroundTask() = viewModelScope.launch {
+
+            try {
+                backgroundWorkCanceled = false
+                delay( 3000 )
+            } catch( e : CancellationException ) {
+                backgroundWorkCanceled = true
+            }
+        }
+
+        fun doMultipleBackgroundTaskWithOneFailing() = with( viewModelScope ) {
+
+            backgroundWorkCompleted = false
+
+            launch {
+                delay( 1000 )
+                throw AssertionError()
+            }
+
+            launch {
+                delay( 2000 )
+                backgroundWorkCompleted = true
+            }
+        }
 
     }
 
@@ -45,5 +78,31 @@ class AbsScopedViewModelTest {
         assert( !viewModel.isScopeActive ) {
             "viewModelScope is active after destroy"
         }
+    }
+
+    @Test
+    fun `AbsScopedViewModel should cancel coroutines on destroy`() {
+
+        runBlocking<Unit> {
+            viewModel.doSomeCancelableBackgroundTask()
+            controller.destroy()
+        }
+
+        assert( viewModel.backgroundWorkCanceled ) {
+            "Background work is not canceled after destroy"
+        }
+    }
+
+    @Test
+    fun `AbsScopedViewModel should use SupervisorJob for unidirectional flow for exceptions`() {
+
+        runBlocking {
+            viewModel.doMultipleBackgroundTaskWithOneFailing().join()
+        }
+
+        assert( viewModel.backgroundWorkCompleted ) {
+            "Sibling coroutines are terminated on a failure"
+        }
+
     }
 }
