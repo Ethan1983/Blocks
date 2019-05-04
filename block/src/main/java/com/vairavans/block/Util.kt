84a1dbb33678b5f2f54e8870a96bd1ca3455f736
Vairavan Srinivasan <vairav@codeaurora.org>
2019-05-04 10:47:21 -0400

    Add repeatUtil to support break

--- block/src/main/java/com/vairavans/block/Util.kt
+++ block/src/main/java/com/vairavans/block/Util.kt
@@ -1,5 +1,8 @@
 package com.vairavans.block
 
+/**
+ * Repeat specified [block] forever. [block] receives a zero based index representing the iteration count
+ */
 inline fun repeatForever( block : (Int) -> Unit ) {
 
     var index = 0
@@ -7,4 +10,28 @@ inline fun repeatForever( block : (Int) -> Unit ) {
     while( true ) {
         block(index++)
     }
+}
+
+/**
+ * Repeat specified [block] until it returns false or specified number of [times].
+ * Use this to break out of a repeat.
+ */
+inline fun repeatUntil( times: Int, block: (Int) -> Boolean ) {
+
+    for( index in 0 until times ) {
+        if( !block(index) ) {
+            break
+        }
+    }
+}
+
+/**
+ * Repeat specified [block] until it returns false. [block] receives a zero based index representing the iteration
+ * count. Use this to break out of a repeat.
+ */
+inline fun repeatUntil( block: (Int) -> Boolean ) {
+
+    var index = 0
+
+    while( block(index++) ) {}
 }
--- block/src/test/java/com/vairavans/block/UtilTest.kt
+++ block/src/test/java/com/vairavans/block/UtilTest.kt
@@ -17,4 +17,89 @@ class UtilTest {
 
         assert( false )
     }
+
+    @Test
+    fun `repeatUntil(times) breaks as expected`() {
+
+        var latestValue = 0
+        var iterationCount = 0
+
+        repeatUntil( 10 ) {
+            iterationCount++
+            val continueIteration = it <= 5
+
+            if( continueIteration ) {
+                latestValue = it
+            }
+            continueIteration
+        }
+
+        assert( latestValue == 5 ) {
+            "repeatUntil continued post break point"
+        }
+
+        assert( iterationCount == 7 ) {
+            "block not invoked expected times"
+        }
+    }
+
+    @Test
+    fun `repeatUntil(times) behaves as repeat when block returns true`() {
+
+        var latestValue = 0
+        var iterationCount = 0
+
+        repeatUntil( 10 ) {
+            iterationCount++
+            latestValue = it
+            true
+        }
+
+        assert( latestValue == 9 ) {
+            "repeatUntil quite earlier"
+        }
+
+        assert( iterationCount == 10 ) {
+            "block not invoked expected times"
+        }
+    }
+
+    @Test
+    fun `repeatUntil breaks as expected`() {
+
+        var latestValue = 0
+        var iterationCount = 0
+
+        repeatUntil {
+            iterationCount++
+            val continueIteration = it <= 5
+
+            if( continueIteration ) {
+                latestValue = it
+            }
+            continueIteration
+        }
+
+        assert( latestValue == 5 ) {
+            "repeatUntil continued post break point"
+        }
+
+        assert( iterationCount == 7 ) {
+            "block not invoked expected times"
+        }
+    }
+
+    @Test
+    fun `repeatUntil behaves as repeat when block returns true`() {
+
+        repeatUntil {
+            assert( true )
+            true
+            return
+        }
+
+        assert( false ) {
+            "repeatUntil failed to loop forever"
+        }
+    }
 }
