370c4e2068a37948ba33b62581cd164fc2185b16
Vairavan Srinivasan <vairav@codeaurora.org>
2019-05-11 11:08:13 -0400

    Add stateSafeNavigate extension on NavController to perform lifecycle safe navigation

---
+++ block/src/main/java/com/vairavans/block/NavController.kt
@@ -0,0 +1,97 @@
+package com.vairavans.block
+
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+
+// Do not use Kotlin defaults to avoid function overloads as the relayed overload logic is an implementation detail
+// of NavController and can change in future.
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle: Lifecycle,
+                                    @IdRes resID : Int ) {
+    lifecycle.startedStateInvoke {
+        navigate( resID )
+    }
+}
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle: Lifecycle,
+                                    @IdRes resID : Int,
+                                    args : Bundle? = null ) {
+    lifecycle.startedStateInvoke {
+        navigate( resID, args )
+    }
+}
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle: Lifecycle,
+                                    @IdRes resID : Int,
+                                    args : Bundle? = null,
+                                    navOptions : NavOptions? = null ) {
+    lifecycle.startedStateInvoke {
+        navigate( resID, args, navOptions )
+    }
+}
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle: Lifecycle,
+                                    @IdRes resID : Int,
+                                    args : Bundle? = null,
+                                    navOptions : NavOptions? = null,
+                                    navigatorExtras : Navigator.Extras? = null ) {
+    lifecycle.startedStateInvoke {
+        navigate( resID, args, navOptions, navigatorExtras )
+    }
+}
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle : Lifecycle,
+                                    directions : NavDirections ) {
+    lifecycle.startedStateInvoke {
+        navigate( directions )
+    }
+}
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle : Lifecycle,
+                                    directions : NavDirections,
+                                    navOptions : NavOptions? = null ) {
+    lifecycle.startedStateInvoke {
+        navigate( directions, navOptions )
+    }
+}
+
+/**
+ * Same as [NavController.navigate] but throws an [IllegalStateException] when lifecycle is not at least
+ * [Lifecycle.State.STARTED] state.
+ */
+fun NavController.stateSafeNavigate(lifecycle : Lifecycle,
+                                    directions : NavDirections,
+                                    navigatorExtras : Navigator.Extras ) {
+    lifecycle.startedStateInvoke {
+        navigate( directions, navigatorExtras )
+    }
+}
---
+++ block/src/test/java/com/vairavans/block/NavControllerTest.kt
@@ -0,0 +1,230 @@
+package com.vairavans.block
+
+import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigator
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.lang.IllegalStateException
+
+@RunWith(JUnit4::class)
+class NavControllerTest {
+
+    private val navController = mockk<NavController>( relaxed = true )
+    private val lifeCycle = mockk<Lifecycle>()
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId fails in INITIALIZED State`() {
+        callStateSafeNavigateResId( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId fails in CREATED State`() {
+        callStateSafeNavigateResId( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId works as expected in STARTED State`() {
+        callStateSafeNavigateResId( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId works as expected in RESUMED State`() {
+        callStateSafeNavigateResId( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId fails in DESTROYED State`() {
+        callStateSafeNavigateResId( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateResId( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, 12 )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle fails in INITIALIZED State`() {
+        callStateSafeNavigateResIdAndBundle( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle fails in CREATED State`() {
+        callStateSafeNavigateResIdAndBundle( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId Bundle works as expected in STARTED State`() {
+        callStateSafeNavigateResIdAndBundle( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId Bundle works as expected in RESUMED State`() {
+        callStateSafeNavigateResIdAndBundle( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle fails in DESTROYED State`() {
+        callStateSafeNavigateResIdAndBundle( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateResIdAndBundle( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, 12, Bundle() )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle Options fails in INITIALIZED State`() {
+        callStateSafeNavigateResIdBundleAndNavOptions( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle Options fails in CREATED State`() {
+        callStateSafeNavigateResIdBundleAndNavOptions( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId Bundle Options works as expected in STARTED State`() {
+        callStateSafeNavigateResIdBundleAndNavOptions( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId Bundle Options works as expected in RESUMED State`() {
+        callStateSafeNavigateResIdBundleAndNavOptions( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle Options fails in DESTROYED State`() {
+        callStateSafeNavigateResIdBundleAndNavOptions( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateResIdBundleAndNavOptions( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, 12, Bundle(), navOptions = null )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle Options Extras fails in INITIALIZED State`() {
+        callStateSafeNavigateResIdBundleNavOptionsAndNavExtras( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle Options Extras fails in CREATED State`() {
+        callStateSafeNavigateResIdBundleNavOptionsAndNavExtras( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId Bundle Options Extras works as expected in STARTED State`() {
+        callStateSafeNavigateResIdBundleNavOptionsAndNavExtras( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate resId Bundle Options Extras works as expected in RESUMED State`() {
+        callStateSafeNavigateResIdBundleNavOptionsAndNavExtras( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate resId Bundle Options Extras fails in DESTROYED State`() {
+        callStateSafeNavigateResIdBundleNavOptionsAndNavExtras( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateResIdBundleNavOptionsAndNavExtras( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, 12, Bundle(), navOptions = null, navigatorExtras = null )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions in INITIALIZED State`() {
+        callStateSafeNavigateDirections( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions fails in CREATED State`() {
+        callStateSafeNavigateDirections( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate directions works as expected in STARTED State`() {
+        callStateSafeNavigateDirections( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate directions works as expected in RESUMED State`() {
+        callStateSafeNavigateDirections( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions fails in DESTROYED State`() {
+        callStateSafeNavigateDirections( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateDirections( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, mockk<NavDirections>() )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions Options in INITIALIZED State`() {
+        callStateSafeNavigateDirectionsAndOptions( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions Options fails in CREATED State`() {
+        callStateSafeNavigateDirectionsAndOptions( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate directions Options works as expected in STARTED State`() {
+        callStateSafeNavigateDirectionsAndOptions( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate directions Options works as expected in RESUMED State`() {
+        callStateSafeNavigateDirectionsAndOptions( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions Options fails in DESTROYED State`() {
+        callStateSafeNavigateDirectionsAndOptions( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateDirectionsAndOptions( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, mockk<NavDirections>(), null )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions Extras in INITIALIZED State`() {
+        callStateSafeNavigateDirectionsAndExtras( Lifecycle.State.INITIALIZED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions Extras fails in CREATED State`() {
+        callStateSafeNavigateDirectionsAndExtras( Lifecycle.State.CREATED )
+    }
+
+    @Test
+    fun `stateSafeNavigate directions Extras works as expected in STARTED State`() {
+        callStateSafeNavigateDirectionsAndExtras( Lifecycle.State.STARTED )
+    }
+
+    @Test
+    fun `stateSafeNavigate directions Extras works as expected in RESUMED State`() {
+        callStateSafeNavigateDirectionsAndExtras( Lifecycle.State.RESUMED )
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `stateSafeNavigate directions Extras fails in DESTROYED State`() {
+        callStateSafeNavigateDirectionsAndExtras( Lifecycle.State.DESTROYED )
+    }
+
+    private fun callStateSafeNavigateDirectionsAndExtras( state : Lifecycle.State ) {
+        every { lifeCycle.currentState } returns state
+        navController.stateSafeNavigate( lifeCycle, mockk(), mockk<Navigator.Extras>() )
+    }
+}
