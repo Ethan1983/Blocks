e05137752311946d14c494ebdd2cb44ca2117691
Vairavan Srinivasan <vairav@codeaurora.org>
2019-05-11 10:33:07 -0400

    Add extension startedStateInvoke on Lifecycle to ensure safe UI updates

---
+++ block/src/main/java/com/vairavans/block/Lifecycle.kt
@@ -0,0 +1,20 @@
+package com.vairavans.block
+
+import androidx.lifecycle.Lifecycle
+
+/**
+ * Extension on [Lifecycle] to invoke specified block only if it is in a [Lifecycle.State.STARTED] state
+ * and throw an [IllegalStateException] otherwise.
+ *
+ * Use this to perform UI updates to ensure they can be restored, if needed. UI updates in a pre-started state
+ * will be lost should the process get killed and restored later.
+ */
+fun Lifecycle.startedStateInvoke( block : () -> Unit ) {
+
+    if( currentState.isAtLeast( Lifecycle.State.STARTED ) ) {
+        block()
+    } else {
+        //Lifecycle is always stopped before state is saved, onSaveInstanceState()
+        throw IllegalStateException( "Navigate request with STOPPED lifecycle state" )
+    }
+}
---
+++ block/src/test/java/com/vairavans/block/LifecycleTest.kt
@@ -0,0 +1,65 @@
+package com.vairavans.block
+
+import androidx.lifecycle.Lifecycle
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class LifecycleTest {
+
+    @Test(expected = IllegalStateException::class)
+    fun `Lifecycle startedStateInvoke fails in INITIALIZED state`() =
+        verifyFailingLifecycleStateInvoke( Lifecycle.State.INITIALIZED )
+
+    @Test(expected = IllegalStateException::class)
+    fun `Lifecycle startedStateInvoke fails in CREATED state`() =
+        verifyFailingLifecycleStateInvoke( Lifecycle.State.CREATED )
+
+    @Test
+    fun `Lifecycle startedStateInvoke works in STARTED state`() =
+        verifySuccessLifecycleStateInvoke( Lifecycle.State.STARTED )
+
+    @Test
+    fun `Lifecycle startedStateInvoke fails in RESUMED state`() =
+        verifySuccessLifecycleStateInvoke( Lifecycle.State.RESUMED )
+
+    @Test(expected = IllegalStateException::class)
+    fun `Lifecycle startedStateInvoke fails in DESTROYED state`() =
+        verifyFailingLifecycleStateInvoke( Lifecycle.State.DESTROYED )
+
+    private fun verifySuccessLifecycleStateInvoke( state : Lifecycle.State ) {
+
+        val lifeCycle = mockk<Lifecycle>()
+        every { lifeCycle.currentState } returns Lifecycle.State.STARTED
+
+        var lambdaInvoked = false
+
+        lifeCycle.startedStateInvoke {
+            lambdaInvoked = true
+        }
+
+        // startedStateInvoke should not throw an exception
+        assert(lambdaInvoked) {
+            "lambda to startedStateInvoke not invoked"
+        }
+    }
+
+    private fun verifyFailingLifecycleStateInvoke(state : Lifecycle.State ) {
+
+        val lifeCycle = mockk<Lifecycle>()
+        every { lifeCycle.currentState } returns state
+
+        var lambdaNotInvoked = true
+
+        lifeCycle.startedStateInvoke {
+            lambdaNotInvoked = false
+        }
+
+        assert(lambdaNotInvoked) {
+            "lambda to startedStateInvoke invoked"
+        }
+    }
+}
